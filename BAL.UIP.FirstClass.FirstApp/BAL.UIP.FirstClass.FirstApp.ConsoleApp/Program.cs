502d822f82ce01bddd0a0374ada6a028055640b1
Oleksandr Badyornyi <badyornyi.o@gmail.com>
2019-02-26 00:38:34 +0200

    first commit

---
+++ BAL.UIP.FirstClass.FirstApp/BAL.UIP.FirstClass.FirstApp.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BAL.UIP.FirstClass.FirstApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int rectangleSideA = 30;
+            //int rectangleSideB = 40;
+
+            //int rectangleSquare = rectangleSideA * rectangleSideB;
+            //int rectanglePerimeter = 2 * rectangleSideA + 2 * rectangleSideB;
+
+            //int i = 0;
+            //int i1 = i--;
+            //Console.WriteLine(i1);
+            //Console.WriteLine(i);
+            //int i2 = --i;
+            //Console.WriteLine(i2);
+
+
+
+            //Console.WriteLine(rectangleSquare);
+            //Console.WriteLine(rectanglePerimeter);
+            //Console.WriteLine(i1);
+            //Console.WriteLine(i2);
+            //Console.ReadLine();
+
+            int personAge = 19;
+
+            bool isAlcoholSaleRestricted = personAge < 21;
+            Console.WriteLine("Alcohol sale restricted: " + isAlcoholSaleRestricted);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ BAL.UIP.HW1.AppleShop/BAL.UIP.HW1.AppleShop.UI.ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BAL.UIP.HW1.AppleShop.UI.ConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BAL.UIP.HW1.AppleShop.UI.ConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c76471bf-4b27-4c7c-91c7-71682b35f441")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
