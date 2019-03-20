755f76e549b54fc1f21a5ecf852f8ce00b435a98
Oleksandr Badyornyi <badyornyi.o@gmail.com>
2019-03-21 00:10:48 +0200

    homework 3 initial commit

---
+++ BAL.UIP.HW3.ArraysLoopsConditionalsMethods/BAL.UIP.HW3.ArraysLoopsConditionalsMethods.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.UIP.HW3.ArraysLoopsConditionalsMethods.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
