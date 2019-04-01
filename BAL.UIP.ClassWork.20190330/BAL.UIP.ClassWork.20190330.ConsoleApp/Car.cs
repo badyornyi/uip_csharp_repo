fea9badc1d79ab07ada4bdd35cf70a8c99655486
Oleksandr Badyornyi <badyornyi.o@gmail.com>
2019-04-01 13:48:48 +0300

    class work, Plant simulator

---
+++ BAL.UIP.ClassWork.20190330/BAL.UIP.ClassWork.20190330.ConsoleApp/Car.cs
@@ -0,0 +1,6 @@
+﻿namespace BAL.UIP.ClassWork._20190330.ConsoleApp
+{
+    public class Car
+    {
+    }
+}
---
+++ BAL.UIP.ClassWork.20190330/BAL.UIP.ClassWork.20190330.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.UIP.ClassWork._20190330.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country = new Country("Ukraine");
+            List<Order> orders = country.GenerateNewOrders(20);
+
+            List<Worker> workers = Country.HireWorkers(5);
+
+            Plant plant = new Plant("Power Plant", workers, country);
+
+            plant.StartNewWorkingYear();
+
+            Console.ReadLine();
+        }
+    }
+}
