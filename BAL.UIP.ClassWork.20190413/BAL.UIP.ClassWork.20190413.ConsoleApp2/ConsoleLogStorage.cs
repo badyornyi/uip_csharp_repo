33b7c9882b7eab8332aa1108d31f55cb3516f785
Oleksandr Badyornyi <badyornyi.o@gmail.com>
2019-04-16 01:19:06 +0300

    1st: Interfaces; 2nd: Logger

---
+++ BAL.UIP.ClassWork.20190413/BAL.UIP.ClassWork.20190413.ConsoleApp2/ConsoleLogStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BAL.UIP.ClassWork._20190413.ConsoleApp2
+{
+    public class ConsoleLogStorage : ILogStorage
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine($"{this.GetType().Name}: {message}");
+        }
+    }
+}
---
+++ BAL.UIP.ClassWork.20190413/BAL.UIP.ClassWork.20190413.ConsoleApp2/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace BAL.UIP.ClassWork._20190413.ConsoleApp2
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Warn(string message);
+        void Error(string message);
+        void Debug(string message);
+        void Fatal(string message);
+    }
+}
